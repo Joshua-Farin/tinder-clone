01caf3128a01c731ce90b8a8cda33724c3016812
Joshua-Farin <joshua.k.farin@gmail.com>
2022-04-06 03:05:19 -1000

    add header

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import { IconButton } from '@mui/material';
+
+function Header() {
+  return (
+    <div className='header'>
+      <IconButton>
+        <PersonIcon fontSize='large' className='header__icon' />
+      </IconButton>
+
+      <img 
+        className='header__logo'
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-emblem.jpg"
+        alt=''
+      />
+
+      
+    </div>
+  )
+}
+
+export default Header
